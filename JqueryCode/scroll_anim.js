2d25b191cbf7ee16093ad656ceca71b44a191347
Ravikant Tyagi <dhruvasharma1989@gmail.com>
2020-08-10 19:44:49 +0530

    Add files via upload

---
+++ JqueryCode/scroll_anim.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+    $("header").css({
+        'transform':'translateY(0%)',
+        'opacity':1
+    });
+    $("#banner").css({
+        'opacity':1
+    });
+});
+
+$(window).scroll(function () { 
+    var scrollValue = $(this).scrollTop();
+    console.log(scrollValue);
+    if(scrollValue > 150) {
+        // $("header").css({'position':'fixed'});
+        // $("header").css({'top':'-200px'});
+        $("header").addClass('fix');
+    }
+    else {
+        // $("header").css({'position':'static'});
+        // $("header").css({'top':'0px'});
+        $("header").removeClass('fix');
+    }
+
+    if(scrollValue > 225) {
+        $('.row').addClass('translate');
+    }
+    else {
+        $('.row').removeClass('translate');
+    }
+});
